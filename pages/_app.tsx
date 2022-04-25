0a29e6030e94e4644805c0c2eed4e153203d82a7
Coding Error <coding.errorr@gmail.com>
2022-04-25 12:05:41 +0600

    first commit

---
+++ pages/_app.tsx
@@ -0,0 +1,31 @@
+import '../styles/globals.css'
+import type { AppProps } from 'next/app'
+import { ClerkProvider, SignedIn, SignedOut, RedirectToSignIn } from '@clerk/nextjs';
+import { useRouter } from 'next/router';
+
+
+const publicPages: string | string[] = [];
+
+function MyApp({ Component, pageProps }: AppProps) {
+
+  const { pathname } = useRouter();
+  const isPublicPage = publicPages.includes(pathname);
+  return (
+    <ClerkProvider>
+      {isPublicPage ? (
+        <Component {...pageProps} />
+      ) : (
+        <>
+          <SignedIn>
+            <Component {...pageProps} />
+          </SignedIn>
+          <SignedOut>
+            <RedirectToSignIn />
+          </SignedOut>
+        </>
+      )}
+    </ClerkProvider>
+  );
+}
+
+export default MyApp
