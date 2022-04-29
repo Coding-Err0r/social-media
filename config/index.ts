dc15f9d262df15d519b71bc1801afe5c99f43fce
Coding Errors <104298251+Coding-Err0r@users.noreply.github.com>
2022-04-29 20:39:19 +0600

    Update index.ts

--- config/index.ts
+++ config/index.ts
@@ -1,4 +1,4 @@
 export const SERVER =
-  'http://social-media-11.herokuapp.com/v1/graphql';
+  'https://social-media-11.herokuapp.com/v1/graphql';
 export const WEB_SOCKET_LINK =
   'ws://social-media-11.herokuapp.com/v1/graphql';
